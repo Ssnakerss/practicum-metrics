d26b29662b77d494c6608a5679a3abf321c27097
Ssnakerss <ivan.krivokon@gmail.com>
2024-07-03 14:53:36 +0300

    tests added

---
+++ cmd/agent/agent_test.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"agent/metric"
+	"testing"
+)
+
+func Test_pollMemStatsMetrics(t *testing.T) {
+	type args struct {
+		metricsToGather []string
+		result          []metric.Metric
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    bool
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := pollMemStatsMetrics(tt.args.metricsToGather, tt.args.result)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("pollMemStatsMetrics() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("pollMemStatsMetrics() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSendMetric(t *testing.T) {
+	type args struct {
+		m metric.Metric
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := SendMetric(tt.args.m); (err != nil) != tt.wantErr {
+				t.Errorf("SendMetric() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
---
+++ cmd/server/storage_test.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"server/metric"
+	"testing"
+)
+
+func TestStorage_Insert(t *testing.T) {
+	type args struct {
+		m metric.Metric
+	}
+	tests := []struct {
+		name    string
+		st      *Storage
+		args    args
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := tt.st.Insert(tt.args.m); (err != nil) != tt.wantErr {
+				t.Errorf("Storage.Insert() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestStorage_Update(t *testing.T) {
+	type args struct {
+		m metric.Metric
+	}
+	tests := []struct {
+		name    string
+		st      *Storage
+		args    args
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := tt.st.Update(tt.args.m); (err != nil) != tt.wantErr {
+				t.Errorf("Storage.Update() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
