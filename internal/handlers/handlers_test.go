eeb007a34c1a4fd932dbffdc0b84753967aecf4b
Ssnakerss <ivan.krivokon@gmail.com>
2024-07-05 16:25:50 +0300

    Update handlers_test.go

--- internal/handlers/handlers_test.go
+++ internal/handlers/handlers_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestUpdateHandler(t *testing.T) {
+func TestChiUpdateHandler(t *testing.T) {
 	type args struct {
 		w http.ResponseWriter
 		r *http.Request
@@ -18,7 +18,7 @@ func TestUpdateHandler(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			UpdateHandler(tt.args.w, tt.args.r)
+			ChiUpdateHandler(tt.args.w, tt.args.r)
 		})
 	}
 }
